a8fba781ab768a05d01912392835b586698f71eb
Justin Newman <newma4jm@dukes.jmu.edu>
2023-09-19 10:26:22 -0400

    SVG update

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -306,67 +306,25 @@ export default function DemoPage() {
                                 >
                                   <span className=" text-gray-500">
                                     {graphic.difficulty === "Easy" ? (
-                                      <svg
-                                        className="h-full w-[16px]"
-                                        viewBox="0 0 22 25"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                      >
-                                        <rect
-                                          y="13.1309"
-                                          width="6"
-                                          height="11"
-                                          rx="1"
-                                          fill="#4E7BBA"
-                                        />
-                                        <rect
-                                          x="8"
-                                          y="8.13086"
-                                          width="6"
-                                          height="16"
-                                          rx="1"
-                                          fill="#E1E1E1"
-                                        />
-                                        <rect
-                                          x="16"
-                                          y="0.130859"
-                                          width="6"
-                                          height="24"
-                                          rx="1"
-                                          fill="#E1E1E1"
-                                        />
-                                      </svg>
+                                    <svg
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    height="1em"
+                                    width="1em"
+                                  >
+                                    <path fill="none" d="M0 0h24v24H0z" />
+                                    <path d="M16 4a1 1 0 011 1v4.2l5.213-3.65a.5.5 0 01.787.41v12.08a.5.5 0 01-.787.41L17 14.8V19a1 1 0 01-1 1H2a1 1 0 01-1-1V5a1 1 0 011-1h14zm-1 2H3v12h12V6zM7.4 8.829a.4.4 0 01.215.062l4.355 2.772a.4.4 0 010 .674L7.615 15.11A.4.4 0 017 14.77V9.23a.4.4 0 01.4-.4zM21 8.84l-4 2.8v.718l4 2.8V8.84z" />
+                                  </svg>
                                     ) : (
                                       <svg
-                                        className="h-full w-[16px]"
-                                        viewBox="0 0 22 25"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                      >
-                                        <rect
-                                          y="13.1309"
-                                          width="6"
-                                          height="11"
-                                          rx="1"
-                                          fill="#4E7BBA"
-                                        />
-                                        <rect
-                                          x="8"
-                                          y="8.13086"
-                                          width="6"
-                                          height="16"
-                                          rx="1"
-                                          fill="#4E7BBA"
-                                        />
-                                        <rect
-                                          x="16"
-                                          y="0.130859"
-                                          width="6"
-                                          height="24"
-                                          rx="1"
-                                          fill="#E1E1E1"
-                                        />
-                                      </svg>
+                                      viewBox="0 0 24 24"
+                                      fill="currentColor"
+                                      height="1em"
+                                      width="1em"
+                                    >
+                                      <path fill="none" d="M0 0h24v24H0z" />
+                                      <path d="M16 4a1 1 0 011 1v4.2l5.213-3.65a.5.5 0 01.787.41v12.08a.5.5 0 01-.787.41L17 14.8V19a1 1 0 01-1 1H2a1 1 0 01-1-1V5a1 1 0 011-1h14zm-1 2H3v12h12V6zM7.4 8.829a.4.4 0 01.215.062l4.355 2.772a.4.4 0 010 .674L7.615 15.11A.4.4 0 017 14.77V9.23a.4.4 0 01.4-.4zM21 8.84l-4 2.8v.718l4 2.8V8.84z" />
+                                    </svg>
                                     )}
                                   </span>
                                   <span className="font-medium text-gray-900">
