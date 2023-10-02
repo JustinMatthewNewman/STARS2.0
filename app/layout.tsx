bea31d9ab99b605443cfe57e2551850ac913c441
Justin Newman <newma4jm@dukes.jmu.edu>
2023-10-02 12:58:14 -0400

    Disable zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <body className="scroll-smooth antialiased [font-feature-settings:'ss01']">
         {children}
       </body>
